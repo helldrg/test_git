2eb1bc7ab2c4434ea3b376b069af549577a83016
Oleg <helldrg13@yandex.ru>
2022-05-18 13:09:47 +0300

    add_text

--- test_git.py
+++ test_git.py
@@ -5,8 +5,12 @@ def search_space(s):
     if s.find(" "):
         return True
 
+def add_text(s):
+    return s + "add text"
+
 s = "Test git"
 
 print(s)
 
 print(search_space(s))
+print(add_text(s))
